68d66ceb01eeaec905a7f976414328d904c6e417
alexander <alexander.upton@kearsarge.org>
2023-05-05 09:31:13 -0400

    .

--- Ball.py
+++ Ball.py
@@ -1,15 +1,14 @@
 import pygame, sys, math
 
 class Ball():
-    def __int__(self, image, speed = [0,0], startpos=[0,0]):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
+    def __init__(self, speed = [0,0], startpos=[0,0]):
+        self.image = pygame.image.load("Ball/ball.png")
+        self.rect = self.image.get_rect(center = startpos)
         self.speedx = speed[0]
         self.speedy = speed[1]
         self.speed = [self.speedx ,self.speedy]
         self.rad = (self.rect.height/2 + self.rect.width/2)/2
         
-        self.rect =self.rect.move(startpos)
         
         self.didbounceX = False
         self.didbounceY = False
@@ -21,10 +20,13 @@ class Ball():
         self.rect = self.rect.move(self.speed)
         
     def wallcollide (self,size):
+        
         width = size [0]
-        height = size [0]
+        height = size [1]
+        print(self.rect.bottom)
         if not self.didbounceY:
             if self.rect.bottom > height:
+                
                 self.speedy = -self.speedy
                 self.didbounceY = True
             if self.rect.top < 0:
@@ -35,10 +37,10 @@ class Ball():
                 self.speedx = -self.speedx
                 self.didbounceX = True
             if self.rect.left < 0:
-                self.speedX = -self.speedX
+                self.speedx = -self.speedx
                 self.didbounceX = True
             
-    def  bllcolide (self, other):
+    def  ballcolide (self, other):
         if self != other:
             if self.rect.right > other.rect.left:
                 if self.rect.lift < other.rect.right:
